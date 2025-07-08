6eaa145e413aa11ed9976f97d94446461fd39275
Alok pradhan <137865136+Alok6371@users.noreply.github.com>
2025-07-08 14:05:25 +0000

    all

---
+++ backend/db/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+Cannot find package 'cloudinary' imported from /workspaces/Chat-App/backend/db/cloudinary.js
+export default cloudinary;
